de309d454859cb9905e065edb99bfd1af1233c03
Sined <147731856+Sined-Htims@users.noreply.github.com>
2024-07-16 15:19:47 +0300

    Create setup_proxy.js

---
+++ frontend/src/setup_proxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// const dom = process.env.REACT_APP_DOMAIN ? process.env.REACT_APP_DOMAIN : "backend";
+
+module.exports = function (app) {
+  app.use(
+    '/backend',
+    createProxyMiddleware({
+      target: http://127.0.0.1:8000,
+      changeOrigin: true,
+      pathRewrite: function (path, req) { return path.replace('/backend', '') }
+    })
+  );
+};
